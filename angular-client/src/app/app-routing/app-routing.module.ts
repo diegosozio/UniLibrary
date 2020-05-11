a51124b6833efd50888cb13337c18edf989e146c
Sokol Guri <sokol.guri@studio.unibo.it>
2020-05-11 12:58:56 +0200

    Refactor project structure to unify the implemented code

--- angular-client/src/app/app-routing/app-routing.module.ts
+++ angular-client/src/app/app-routing/app-routing.module.ts
@@ -6,13 +6,14 @@ import { RouterModule } from '@angular/router'
 import { HomeComponent } from '../authentication/home/home.component';
 import { LogInComponent } from '../authentication/log-in/log-in.component';
 import { RegisterComponent } from '../authentication/register/register.component';
+import { ArchiveComponent } from '../archive/archive.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent  },
   { path: 'log-in', component: LogInComponent }, 
   { path: 'register', component: RegisterComponent },
+  { path: 'gallery', component: ArchiveComponent },
 
-  
     // otherwise redirect to home 
     { path: '**', redirectTo: '' }
 ];
--- angular-client/src/app/app.module.ts
+++ angular-client/src/app/app.module.ts
@@ -1,17 +1,20 @@
+import { AppComponent } from './app.component';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing/app-routing.module';
-import { AppComponent } from './app.component';
-import { ArchiveComponent } from './archive/archive.component';
 import { ArchiveModule } from './archive/archive.module';
-import { FilterComponent } from './archive/filter/filter.component';
-import { BooksComponent } from './archive/books/books.component';
-import { BookComponent } from './archive/books/book/book.component';
-//import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 import { HomeComponent } from './authentication/home/home.component';
 import { LogInComponent } from './authentication/log-in/log-in.component';
 import { RegisterComponent } from './authentication/register/register.component';
+import { ArchiveComponent } from './archive/archive.component';
+import { BooksComponent } from './archive/books/books.component';
+import { BookComponent } from './archive/books/book/book.component';
+import { FilterComponent } from './archive/filter/filter.component';
+
 
 @NgModule({
   declarations: [
@@ -28,7 +31,7 @@ import { RegisterComponent } from './authentication/register/register.component'
     BrowserModule,
     AppRoutingModule,
     ArchiveModule,
-    //NgbModule,
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
