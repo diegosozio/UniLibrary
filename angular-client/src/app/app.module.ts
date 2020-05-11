3d6b26615580b283d3ecb86494d27565f9fc8d1e
Sokol Guri <sokol.guri@studio.unibo.it>
2020-05-11 11:14:18 +0200

    removing useless files and updating library versions

--- angular-client/src/app/app.module.ts
+++ angular-client/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 import { AppComponent } from './app.component';
+
+import { HomeComponent } from './authentication/home/home.component';
+import { LogInComponent } from './authentication/log-in/log-in.component';
+import { RegisterComponent } from './authentication/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LogInComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
