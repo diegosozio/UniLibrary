06eb94a35b94fca2c9e76d5771c9e35ddc7b5583
diego <diego.sozio92@gmail.com>
2020-05-15 17:51:15 +0200

    router + 404

--- angular-client/src/app/app-routing/app-routing.module.ts
+++ angular-client/src/app/app-routing/app-routing.module.ts
@@ -7,15 +7,17 @@ import { WelcomeComponent } from '../welcome/welcome.component';
 import { LogInComponent } from '../authentication/log-in/log-in.component';
 import { RegisterComponent } from '../authentication/register/register.component';
 import { ArchiveComponent } from '../archive/archive.component';
-
+import { NotFoundComponent} from '../not-found/not-found.component';
+ 
 const routes: Routes = [  
   { path: '', redirectTo: '/welcome', pathMatch: 'full' },
   { path: 'welcome', component: WelcomeComponent  },
   { path: 'log-in', component: LogInComponent }, 
   { path: 'register', component: RegisterComponent },
   { path: 'gallery', component: ArchiveComponent },
+  { path: 'notfound', component: NotFoundComponent  },
+  { path: '**', redirectTo: '/notfound', pathMatch: 'full' },
 
-  {path: '**', redirectTo: '404'},
 ];
   
 
